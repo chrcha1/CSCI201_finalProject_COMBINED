3f379ac421f86082c4f95ee15a71d0b2281ff513
Reza Banankhah <banankha@usc.edu>
2023-11-30 21:59:02 -0800

    Updated user model to reflect database

--- smartScheduler/src/main/java/models/User.java
+++ smartScheduler/src/main/java/models/User.java
@@ -1,41 +1,30 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Timestamp;
 
 public class User {
-    private String userId;
+    private String id;
     private String username;
-    private String hashedPassword;
-    private String email;
-    private List<Event> participatedEvents;
-    private List<Event> scheduledEvents;
-    private AvailabilityMatrix availabilityMatrix;
+    private String password;
+    private Timestamp createdAt;
 
     // Constructors
 
-    public User(String userId, String username, String hashedPassword) {
-        this.userId = userId;
+    public User(String id, String username, String password, Timestamp createdAt) {
+        this.id = id;
         this.username = username;
-        this.hashedPassword = hashedPassword;
-        this.email = ""; // Initialize email to an empty string
-        this.participatedEvents = new ArrayList<>();
-        this.scheduledEvents = new ArrayList<>();
-        this.availabilityMatrix = new AvailabilityMatrix();
-    }
-
-    public void updateAvailabilityMatrix(AvailabilityMatrix newAvailabilityMatrix) {
-        this.availabilityMatrix.updateAvailability(this, newAvailabilityMatrix);
+        this.password = password;
+        this.createdAt = createdAt;
     }
 
     // Getters and Setters
 
     public String getUserId() {
-        return userId;
+        return id;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setUserId(String id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -47,90 +36,14 @@ public class User {
     }
 
     public String getHashedPassword() {
-        return hashedPassword;
+        return password;
     }
 
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
+    public void setHashedPassword(String password) {
+        this.password = password;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Event> getParticipatedEvents() {
-        return participatedEvents;
-    }
-
-    public void setParticipatedEvents(List<Event> participatedEvents) {
-        this.participatedEvents = participatedEvents;
-    }
-
-    public List<Event> getScheduledEvents() {
-        return scheduledEvents;
-    }
-
-    public void setScheduledEvents(List<Event> scheduledEvents) {
-        this.scheduledEvents = scheduledEvents;
-    }
-
-    public void setTimeSlot(int dayIndex, int hourIndex, boolean isAvailable) {
-        this.availabilityMatrix.updateAvailability(this, dayIndex, hourIndex, isAvailable);
-    }
-
-    public void unsetTimeSlot(int dayIndex, int hourIndex) {
-        this.availabilityMatrix.updateAvailability(this, dayIndex, hourIndex, false);
-    }
-
-    // Additional methods as needed
-
-    public void addTimeSlot(int dayIndex, int hourIndex, boolean isAvailable) {
-        this.availabilityMatrix.updateAvailability(this, dayIndex, hourIndex, isAvailable);
-    }
-
-    public void removeTimeSlot(int dayIndex, int hourIndex) {
-        this.availabilityMatrix.updateAvailability(this, dayIndex, hourIndex, false);
-    }
-
-    public void participateInEvent(Event event) {
-        this.participatedEvents.add(event);
-    }
-
-    public void scheduleEvent(Event event) {
-        this.scheduledEvents.add(event);
-    }
-
-    public boolean isAvailable(int dayIndex, int hourIndex) {
-        // Check if the user is available at the given matrix spot
-        return availabilityMatrix.isUserAvailable(this, dayIndex, hourIndex);
-    }
-
-    public List<Integer> getAvailableMatrixIndices() {
-        return availabilityMatrix.getFilledMatrixIndices(this);
-    }
-
-    public AvailabilityMatrix getAvailabilityMatrix() {
-        return availabilityMatrix;
-    }
-
-    public void printAvailabilityMatrix() {
-        System.out.println("Availability Matrix for User " + getUserId() + ":");
-        Boolean[][] matrix = availabilityMatrix.getMatrixForUser(this);
-        if (matrix != null) {
-            for (int dayIndex = 0; dayIndex < 7; dayIndex++) {
-                for (int hourIndex = 0; hourIndex < 24; hourIndex++) {
-                    System.out
-                            .print((matrix[dayIndex][hourIndex] != null ? matrix[dayIndex][hourIndex] : "null") + " ");
-                }
-                System.out.println(); // Move to the next line for the next day
-            }
-        } else {
-            System.out.println("Matrix is null");
-        }
-        System.out.println();
+    
+    public Timestamp getCreationTime() {
+    	return createdAt;
     }
 }
