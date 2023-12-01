758bd45a9d922ed33d401a6fe594ec3f69abef17
Reza Banankhah <banankha@usc.edu>
2023-11-30 18:52:58 -0800

    Created login servlet

---
+++ smartScheduler/src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,68 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import util.DatabaseUtil;
+import util.ValidationUtil;
+
+import com.google.gson.JsonObject;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("application/json");
+    	response.setCharacterEncoding("UTF-8");
+		
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        JsonObject jsonResponse = new JsonObject();
+        
+        if (ValidationUtil.isNullOrEmpty(username) || ValidationUtil.isNullOrEmpty(password)) {
+            jsonResponse.addProperty("error", "Username and password cannot be empty.");
+            return;
+        }
+        
+        try {
+            Connection conn = DatabaseUtil.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", username);
+                jsonResponse.addProperty("success", true);
+                jsonResponse.addProperty("redirect", "html/home.html");
+                
+            } else {
+                jsonResponse.addProperty("error", "Invalid username or password.");
+            }
+        } catch (SQLException e) {
+        	jsonResponse.addProperty("error", "Database connection problem: " + e.getMessage());
+        } finally {
+        	response.getWriter().write(jsonResponse.toString());
+        }
+    }
+}
